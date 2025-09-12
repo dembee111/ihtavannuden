c225cc7036384178ad3bd32ce7e563a0bd628ba5
tumenjargal ijilee <tumenjargalijilee@MacBook-Pro.local>
2025-09-12 18:01:02 +0800

    fff

--- src/components/AboutPage/DoYou.tsx
+++ src/components/AboutPage/DoYou.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const DoYou = () => {
   return (
-    <div className="bg-[#f7c51e] px-[50px] ">
-      <h1>Та бидэ дизайны туршлага хайж байна уу?</h1>
+    <div className="bg-[#f7c51e] py-[50px] ">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-4xl font-bold">
+            Та бидэнтэй холбогдоорой !
+          </h1>
+          <Button
+            asChild
+            className="w-52 py-5 bg-transparent border-2 border-white rounded-none hover:bg-[#f7c51e] hover:border-[#f7c51e] transition-colors"
+          >
+            <Link href="/work">Бүгдийг үзэх</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
