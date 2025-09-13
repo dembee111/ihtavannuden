734f308f40352cfb49f1e9e645718899adb7a31c
tumenjargal ijilee <tumenjargalijilee@MacBook-Pro.local>
2025-09-13 11:36:32 +0800

    addd

---
+++ src/components/ContactPage/index.tsx
@@ -0,0 +1,47 @@
+const ContactPage = () => {
+  return (
+    <section className="my-24 2xl:px-0 px-4">
+      <div className="max-w-6xl mx-auto">
+        <div>map</div>
+        <div className="grid grid-cols-12 items-start">
+          <div className="col-span-12 lg:col-span-6">
+            <h1>Бидэнтэй холбоо барина уу</h1>
+            <p>
+              Lorem ipsum gravida nibh vel velit auctor aliqunean
+              sollicitudinlor quisbibendum auci elit consequat ipsutis sem nibh
+              id elitsed vulputate.
+            </p>
+            <div className="flex justify-start gap-2 items-center ">
+              <div>Facebook </div>
+              <div>Instagram </div>
+              <div>Youtube </div>
+            </div>
+            <div className="flex justify-start gap-2 items-center">
+              <div>Image Phone icon</div>
+              <div>
+                <div>Phone :</div>
+                <div>+62828 256 8675</div>
+              </div>
+            </div>
+            <div className="flex justify-start gap-2 items-center">
+              <div>Image Phone icon</div>
+              <div>
+                <div>Phone :</div>
+                <div>+62828 256 8675</div>
+              </div>
+            </div>
+            <div className="flex justify-start gap-2 items-center">
+              <div>Image Phone icon</div>
+              <div>
+                <div>Phone :</div>
+                <div>+62828 256 8675</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactPage;
