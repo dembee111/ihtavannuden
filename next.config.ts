05f22e9b738f9bd15b94e1adac9bb4d65e91f0b4
tumenjargal ijilee <tumenjargalijilee@MacBook-Pro.local>
2026-01-23 11:24:37 +0800

    add

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     formats: ["image/avif", "image/webp"],
-//     domains: ["images.ctfassets.net"], // Contentful-ийн домэйн
-//   },
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
-    domains: ["images.ctfassets.net"],
-  },
-  env: {
-    CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-    CONTENTFUL_INPUT_TOKEN: process.env.CONTENTFUL_INPUT_TOKEN,
-    NEXT_PUBLIC_INPUT_TOKEN: process.env.NEXT_PUBLIC_INPUT_TOKEN,
+    domains: ["images.ctfassets.net"], // Contentful-ийн домэйн
   },
 };
 
